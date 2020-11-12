ae81dae3fce921294e9e64b5b271a6b877646c37
sanyog shakya <shakyasanyog01@gmail.com>
2020-11-12 15:49:49 +0545

    made the edit button fully functional and debugged the app

--- script.js
+++ script.js
@@ -1,48 +1,58 @@
-let todoInput = document.querySelector(".todo-input");
-let addButton = document.querySelector(".todo-add-btn");
-let listContainer = document.querySelector(".todo-list-container");
-let clearButton = document.querySelector(".clear-all-btn");
+const todoInput = document.querySelector(".todo-input");
+const addButton = document.querySelector(".todo-add-btn");
+const listContainer = document.querySelector(".todo-list-container");
+const clearButton = document.querySelector(".clear-all-btn");
 
 addButton.addEventListener('click', addTodo);
 listContainer.addEventListener('click', removeCheckEdit);
 clearButton.addEventListener('click', clearList);
 
 // Add Items to the Todo List
-function addTodo(event) {
+function addTodo(e) {
 
-    event.preventDefault();
+    e.preventDefault();
     
     if (todoInput.value !== "") {
          // Todo List div
         const todoListElements = document.createElement('div');
-        todoListElements.classList.add("todo-list-elements");
+        todoListElements.className = "todo-list-elements";
         
         // Div for input value and check button
         const checkItem = document.createElement('div');
-        checkItem.classList.add('check-item');
+        checkItem.className = 'check-item';
         
         // Check Button
         const checkButton = document.createElement('button');
-        checkButton.classList.add('check-btn');
+        checkButton.className = 'check-btn';
         checkButton.innerHTML = '<i class="fas fa-check"></i>';
         
         // Item
         const newItem = document.createElement('li');
-        newItem.classList.add('list-item');
+        newItem.className = 'list-item';
         newItem.innerText = todoInput.value;
+
+        const textEdit = document.createElement('input');
+        textEdit.type = 'text';
+        textEdit.className = 'text-edit';
+        textEdit.value = todoInput.value;
         
         // Div for Edit and Trash Buttons
         const editTrash = document.createElement('div');
-        editTrash.classList.add('edit-trash');
+        editTrash.className ='edit-trash';
         
         // Edit button
         const editButton = document.createElement('button');
-        editButton.classList.add('edit-btn');
+        editButton.className = 'edit-btn';
         editButton.innerHTML = '<i class="fas fa-edit"></i>';
+
+        // Save Button 
+        const saveButton = document.createElement('button');
+        saveButton.className = 'save-btn';
+        saveButton.innerHTML = '<i class="fas fa-save"></i>';
         
         // Trash button
         const trashButton = document.createElement('button');
-        trashButton.classList.add('trash-btn');
+        trashButton.className = 'trash-btn';
         trashButton.innerHTML = '<i class="fas fa-trash"></i>';
         
 
@@ -51,7 +61,9 @@ function addTodo(event) {
         todoListElements.appendChild(editTrash);
         checkItem.appendChild(checkButton);
         checkItem.appendChild(newItem);
+        checkItem.appendChild(textEdit);
         editTrash.appendChild(editButton);
+        editTrash.appendChild(saveButton);
         editTrash.appendChild(trashButton);
 
         todoInput.value = "";
@@ -69,12 +81,13 @@ function removeCheckEdit(e) {
 
     // remove Item
     if (element.classList[1] === 'fa-trash'){
+        const listContainer = element.parentElement.parentElement.parentElement.parentElement;
         const todoItem = element.parentElement.parentElement.parentElement;
-        listContainer = todoItem.parentElement;
         todoItem.remove();
         if (listContainer.innerHTML === '') {
             clearButton.style.display = "none";
         }
+        
     }
 
     // check Item
@@ -88,10 +101,34 @@ function removeCheckEdit(e) {
 
     // // edit Item
     if (element.classList[1] === 'fa-edit') {
-        const todoListElementsobj = element.parentElement.parentElement.parentElement;
-        const itemValue = todoListElementsobj.childNodes[0].childNodes[1].innerText;
-        todoInput.value = itemValue;
-        console.log(todoListElementsobj);
+        const editButton = element.parentElement;
+        const saveButton = element.parentElement.parentElement.childNodes[1];
+        
+        editButton.style.display = "none";
+        saveButton.style.display = "block";
+
+        const textEdit = saveButton.parentElement.parentElement.childNodes[0].childNodes[2];
+        textEdit.style.display = "block";
+
+        const item = saveButton.parentElement.parentElement.childNodes[0].childNodes[1];
+        item.style.display = "none";
+
+    }
+
+    if (element.classList[1] === 'fa-save') {
+        const saveButton = element.parentElement;
+        const editButton = element.parentElement.parentElement.childNodes[0];
+
+        saveButton.style.display = "none";
+        editButton.style.display = "block";
+
+        const textEdit = saveButton.parentElement.parentElement.childNodes[0].childNodes[2];
+        
+        const item = saveButton.parentElement.parentElement.childNodes[0].childNodes[1];
+        item.innerText = textEdit.value;
+
+        item.style.display = "block";
+        textEdit.style.display = "none";
 
     }
 
@@ -99,8 +136,7 @@ function removeCheckEdit(e) {
 
 function clearList(e) {
     const button = e.target;
-    appContainer = button.parentElement;
-    listContainer = appContainer.childNodes[3];
+    // appContainer = button.parentElement;
     listContainer.innerHTML = "";
     clearButton.style.display = "none";
     todoInput.value = "";
